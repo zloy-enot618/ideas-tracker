6e8cce99f2b515bbac3b3ad238da17c2ad59ba0f
Oleg Markin <o51393680@gmail.com>
2026-09-12 10:58:47 +0300

    Добавил функцию вывода идей

--- modules/ideas.py
+++ modules/ideas.py
@@ -1,5 +1,8 @@
 def show_ideas(ideas):
-    None
+    print("==== Список ваших идей =====")
+    index = 0
+    for index, idea in enumerate(ideas, start=1):
+        print(f"{index}. Имя: {idea['name']} | Тип: {idea['topic']} | Сложность: {idea['difficulty']}")
 
 def add_ideas(ideas, name, topic, difficulty):
     new_idea = {
